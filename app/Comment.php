c2c5763e18077628dc3768053a542d9fb2b81ce1
Mauri de Souza Nunes <mauri870@gmail.com>
2015-10-22 22:37:48 -0200

    Updated relations

--- app/Comment.php
+++ app/Comment.php
@@ -18,7 +18,7 @@ class Comment extends Model
      */
     public function users()
     {
-        return $this->belongsTo('App\User');
+        return $this->belongsTo('App\User','user_id');
     }
 
     /**
